f5e22216a011f9d61dd624415211a1c61005ff61
Tonlito <Antoniow4@hotmail.com>
2025-09-25 10:38:52 +0200

    Added Dto mapp with RegisterRequest.
    Added related fields to program: Datacontext and Identityuser

---
+++ Infrastructure/Dtos/RegisterRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Dtos;
+
+public class RegisterRequest
+{
+
+    [Required]
+
+    public string Firstname { get; set; } = null!;
+
+    [Required]
+
+    public string Lastname { get; set; } = null!;
+
+    [Required]
+
+    public string Email { get; set; } = null!;
+
+    [Required]
+
+    public string Password { get; set; } = null!;
+
+    [Required]
+
+    public string ConfirmedPassword { get; set; } = null!;
+
+}
